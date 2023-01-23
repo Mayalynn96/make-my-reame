31ac2d6d587ecfbe4eb9e14bdd758bd1698f73b2
Maya Lynn <mayalynncohen@gmail.com>
2023-01-22 21:04:16 -0800

    added readme markdown and fs.makefile

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# npmREADMEgenerator
+# ThisTimeIsTheTime 
+        
+## Description
+        
+This should definitly work right?
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+- [Test](#test)
+- [Questions](#questions)
+
+## Installation
+
+N/A
+
+## Usage
+
+Inout on index.js
+
+## Contributing
+
+N/A
+
+## Test
+
+undefined
+
+## Questions
+
+MayaLynn96.gihub.com
+mayalynncohen@gmail.com
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const questions = require('inquirer');
+const fs = require('fs')
 
 questions.prompt([
     {
@@ -14,8 +15,78 @@ questions.prompt([
     {
         type: "input",
         name: "installation",
-        message: "Enter Installation Instructionsc or N/A if there are none"
+        message: "Enter Installation Instructions or N/A if there are none"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please enter Usage Instructions"
+    },
+    {
+        type: "list",
+        name: "liscense",
+        message: "What liscence is your project under?",
+        choices: ["Apache License 2.0", "GNU General Public License v3.0", "MIT License", "BSD 2-Clause 'simplified' License", "BSD 3-Clause 'New' or 'Revised' License", "Boost Software License 1.0", "Creative Commons Zero v1.0 Universal", "Eclipse Public License 2.0", "GNU Affero General Public License v3.0", "GNU General Public License v2.0", "GNU LEsser General Public License v2.1", "Mozilla Public License 2.0", "The Unlicense"]
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Enter Contribution guidelines or N/A if there are none"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Enter Test Instructions or N/A if there are none"
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "Please enter your GitHub profile link"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter your email address"
     }
 ]).then((data) => {
-    console.log(data.projectName + "\n" + "Description: " + data.description + "\n" + "Installastion: : " + data.installation)
+    readmeTextContent =
+ `# ${data.projectName} 
+        
+## Description
+        
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+- [Test](#test)
+- [Questions](#questions)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## Contributing
+
+${data.contributing}
+
+## License
+
+## Test
+
+${data.test}
+
+## Questions
+
+${data.github}
+${data.email}
+`
+fs.writeFile(`README.md`, readmeTextContent, (err) =>
+        err ? console.log(err) : console.log("success!"))
 });
